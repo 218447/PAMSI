f674de5642896266b7f8d4bcac700c0a0a98c139
Tyberiusz <tyberio21@gmail.com>
2016-04-28 15:21:58 +0200

    Sprawko

--- Lab5/src/main.cpp
+++ Lab5/src/main.cpp
@@ -8,19 +8,21 @@ int main () {
   int found;  //zmienna sprawdzajaca czy znaleziono element
   int seeked; //zmienna, ktora szukamy
   double *czas;
-  std::ifstream file ("liscie_10"); 
+  std::ifstream file ("liscie_100"); 
 
   stoper.start();
   file >> Drzewo;
   stoper.stop();
-  
+
+  seeked=4;  
+
   stoper.start();
-  seeked = Drzewo->find(seeked);
+  found = Drzewo->find(seeked);
   stoper.stop();
 
   if (seeked == found) {
     std::cout << "znaleziono" <<std::endl;
   }
   czas=stoper.wyswietl();
-  std::cout << "Czas zapisu dla: " << "10 elementow" << "wynosi: " << czas[0] << std::endl << std::endl << "Czas szukania elementu: " << seeked << "wynosi: " << czas[1]<<std::endl;
+  std::cout << "Czas zapisu dla: " << " 100 elementow " << "wynosi: " << czas[0] << std::endl << std::endl << "Czas szukania elementu: " << seeked << " wynosi: " << czas[1]<<std::endl;
 }
